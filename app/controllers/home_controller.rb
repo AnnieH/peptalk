74b3e80e8aa4769dc4edef9faee4e3d2ccb6bf25
Richard Ortega <richardjortega@gmail.com>
2013-06-02 11:34:15 -0500

    set primary calendar for user and all their events

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -5,11 +5,12 @@ class HomeController < ApplicationController
     if (user_signed_in? )
       client = ClientBuilder.get_client(current_user)
       service = client.discovered_api('calendar', 'v3')
-      result = client.execute(:api_method => service.calendar_list.list)
+      # result = client.execute(:api_method => service.calendar_list.list)
+      # @calendars = result.data
 
-      @calendars = result.data
-      # event_results = client.execute(:api_method => service.)
-      # @events = 
+      event_results = client.execute(:api_method => service.events.list,
+                                     :parameters => {'calendarId' => 'primary'})
+      @events = event_results.data
     end
 
   end
